9072b14d5077c9c0bfd9e9172172670187f31f35
Heber <heber.sistweb@gmail.com>
2019-04-18 23:51:36 -0300

    criando opcao para mover ou copiar

--- ReplaceDirectoryRecursive/MainWindow.xaml.cs
+++ ReplaceDirectoryRecursive/MainWindow.xaml.cs
@@ -27,14 +27,19 @@ namespace ReplaceDirectoryRecursive
         int contaArquivosTotal = 0;
         int contaTotalItens = 0;
         int contaPastaReplace = 0;
+        bool? isCreateCopy = true;
+
+        long sizeFiles = 0;
 
         public delegate void CaminhoCopyHandler(string valor, int ContPasta);
         public event CaminhoCopyHandler PathCopyEvent;
 
         public delegate void ContagemItensHandler(int qtd);
+        public delegate void ContagemTamanhoHandler(long qtd);
         public event ContagemItensHandler ContagemPastasEvent;
         public event ContagemItensHandler ContagemArquivosEvent;
         public event ContagemItensHandler ContagemTotalEvent;
+        public event ContagemTamanhoHandler ContagemTamanhoEvent;
 
         public MainWindow()
         {
@@ -42,6 +47,7 @@ namespace ReplaceDirectoryRecursive
             ContagemPastasEvent += OnContagemPastas;
             ContagemArquivosEvent += OnContagemArquivos;
             ContagemTotalEvent += OnContagemTotal;
+            ContagemTamanhoEvent += OnContagemTamanho;
             PathCopyEvent += OnPathCopy;
         }
 
@@ -63,6 +69,40 @@ namespace ReplaceDirectoryRecursive
         {
             Dispatcher.BeginInvoke((Action)(() =>
                 qdtTotalExibe.Content = qtd
+            ));
+        }
+
+        private void OnContagemTamanho(long qtd)
+        {
+            decimal tamanhoExibe;
+            string tipo = "bytes";
+            decimal div = qtd;
+            if (qtd >= 1024)
+            {
+                div = ((decimal)qtd / 1024);
+                tipo = "KB";
+            }
+
+            if (qtd >= 1048576)
+            {
+                div = ((decimal)div / 1024);
+                tipo = "MB";
+            }
+
+            if (qtd >= 1073741824)
+            {
+                div = ((decimal)div / 1024);
+                tipo = "GB";
+            }
+
+            if (qtd >= 1099511627776)
+            {
+                div = ((decimal)div / 1024);
+                tipo = "TB";
+            }
+            tamanhoExibe = (decimal)Math.Round(div, 2);
+            Dispatcher.BeginInvoke((Action)(() =>
+                sizeFilesExibe.Content = string.Format("{0} ({1})", tamanhoExibe, tipo)
             ));
         }
 
@@ -94,6 +134,7 @@ namespace ReplaceDirectoryRecursive
 
         private void replace_Click(object sender, RoutedEventArgs e)
         {
+            this.isCreateCopy = createCopy.IsChecked;
             if (DiretorioFonte.Text != "" && DiretorioDestino.Text != "")
             {
                 var caminhoDiretorio = DiretorioFonte.Text;
@@ -156,28 +197,32 @@ namespace ReplaceDirectoryRecursive
             }
         }
 
-        private void Inicia(string caminhoDiretorio, string caminhoDestino)
+        private async void Inicia(string caminhoDiretorio, string caminhoDestino)
         {
             contaPastaTotal = 1;
             contaArquivosTotal = 0;
             contaTotalItens = 0;
             contaPastaReplace = 0;
 
-            List<Task> tasks = new List<Task>();
-            tasks.Add(Task.Run(() => ContaDiretorioRecursivo(caminhoDiretorio)));
-            tasks.Add(Task.Run(() => ContaArquivosRecursivo(caminhoDiretorio)));
-            Task.WaitAll(tasks.ToArray());
+            await this.IniciaContagenItens(caminhoDiretorio);
 
             ContagemPastasEvent(contaPastaTotal);
             ContagemArquivosEvent(contaArquivosTotal);
             contaTotalItens = (contaPastaTotal + contaArquivosTotal);
             ContagemTotalEvent(contaTotalItens);
+            ContagemTamanhoEvent(sizeFiles);
 
-            Dispatcher.BeginInvoke((Action)(() =>
+            await Dispatcher.BeginInvoke((Action)(() =>
                andamentoReplace.IsIndeterminate = false
             ));
 
-            Task.Run(() => replaceDiretorioRecursivo(caminhoDiretorio, caminhoDestino));
+            await Task.Run(() => replaceDiretorioRecursivo(caminhoDiretorio, caminhoDestino));
+        }
+
+        public async Task IniciaContagenItens(string caminhoDiretorio)
+        {
+            await Task.Run(() => ContaDiretorioRecursivo(caminhoDiretorio));
+            await Task.Run(() => ContaArquivosRecursivo(caminhoDiretorio));
         }
 
         private void ContaDiretorioRecursivo(string caminhoDiretorio)
@@ -189,10 +234,15 @@ namespace ReplaceDirectoryRecursive
                 DirectoryInfo[] subDir = dirAtua.GetDirectories();
 
                 contaPastaTotal += dirAtua.GetDirectories().Count();
+            
                 foreach (var item in subDir)
                 {
                     this.ContaDiretorioRecursivo(item.FullName);
                 }
+            }
+            else
+            {
+                contaPastaTotal = 0;
             }
         }
 
@@ -204,7 +254,11 @@ namespace ReplaceDirectoryRecursive
                 DirectoryInfo[] subDir = dirAtua.GetDirectories();
 
                 contaArquivosTotal += dirAtua.GetFiles().Count();
-                //ContagemArquivos(contaArquivosTotal);
+
+                foreach(var files in dirAtua.GetFiles())
+                {
+                    sizeFiles += files.Length;
+                }
 
                 foreach (var item in subDir)
                 {
@@ -218,22 +272,54 @@ namespace ReplaceDirectoryRecursive
             if (Directory.Exists(caminhoDiretorio))
             {
                 var vetCaminho = caminhoDiretorio.Split('\\');
-                string novoCaminho = caminhoDestino + "\\" + vetCaminho[vetCaminho.Length - 1].ToString().Replace(" ", "_");
+                string novoCaminho = "";// caminhoDestino + "\\" + vetCaminho[vetCaminho.Length - 1].ToString().Replace(" ", "_");
+                DirectoryInfo dirAtua = new DirectoryInfo(caminhoDiretorio); ;
+                if (this.isCreateCopy == true)
+                {
+                    novoCaminho = caminhoDestino + "\\" + vetCaminho[vetCaminho.Length - 1].ToString().Replace(" ", "_");
+                    Directory.CreateDirectory(novoCaminho);
+                }
+                else
+                {
+                    for (int i = 0; i < vetCaminho.Length; i++)
+                    {
+                        if (string.IsNullOrEmpty(novoCaminho))
+                        {
+                            novoCaminho = vetCaminho[i];
+                        }
+                        else
+                        {
+                            if (i == (vetCaminho.Length - 1))
+                            {
+                                novoCaminho = novoCaminho + "\\" + vetCaminho[i].ToString().Replace(" ", "_");
+                            }
+                            else
+                            {
+                                novoCaminho = novoCaminho + "\\" + vetCaminho[i].ToString();
+                            }
+                        }
+                    }
 
-                Directory.CreateDirectory(novoCaminho);
+                    if (caminhoDiretorio != novoCaminho && novoCaminho.Length < 260)
+                    {
+                        Directory.Move(caminhoDiretorio, novoCaminho);
+                        dirAtua = new DirectoryInfo(novoCaminho);
+                    }
+                }
+
                 contaPastaReplace++;
                 PathCopyEvent(novoCaminho, contaPastaReplace);
 
-                DirectoryInfo dirAtua = new DirectoryInfo(caminhoDiretorio);
                 DirectoryInfo[] subDir = dirAtua.GetDirectories();
 
                 FileInfo[] arquivos = dirAtua.GetFiles();
                 foreach (var fileItem in arquivos)
                 {
                     string caminhoFile = System.IO.Path.Combine(novoCaminho, fileItem.Name);
-                    fileItem.CopyTo(caminhoFile, true);
                     contaPastaReplace++;
                     PathCopyEvent(caminhoFile, contaPastaReplace);
+                    if(this.isCreateCopy == true)
+                        fileItem.CopyTo(caminhoFile, true);
                 }
 
                 foreach (var item in subDir)
